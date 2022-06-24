4f1fae43c12c9e9aa4cff7fafdef7f8e5df539e8
Shenghang Zhu <67240191+zyhzsh@users.noreply.github.com>
2022-06-24 11:17:43 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'S.Zhu Doc',
+  tagline: '......',
   url: 'https://github.com',
   baseUrl: '/s.zhu-document/',
   onBrokenLinks: 'throw',
